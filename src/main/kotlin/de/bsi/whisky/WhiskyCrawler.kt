fde3e4337ae8149cd39ed4446fa73ff0828bbe3d
Elmar Brauch <elmar.brauch@gmail.com>
2022-10-01 23:35:34 +0200

    Version updates

--- src/main/kotlin/de/bsi/whisky/WhiskyCrawler.kt
+++ src/main/kotlin/de/bsi/whisky/WhiskyCrawler.kt
@@ -1,6 +1,7 @@
 package de.bsi.whisky
 
 import org.apache.logging.log4j.LogManager
+import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import org.springframework.stereotype.Service
 import javax.annotation.PreDestroy
@@ -36,12 +37,12 @@ class WhiskyCrawler {
     private fun searchWhiskies(whiskyName: String): Map<String, List<String>> {
         browser.get("https://whiskybase.com/search?q=%s".format(whiskyName))
         val map = mutableMapOf<String, MutableList<String>>()
-        for(whisky in browser.findElementsByXPath("//div[@class='row']//a[@class='clickable']"))
+        for(whisky in browser.findElements(By.xpath("//div[@class='row']//a[@class='clickable']")))
             map.putIfAbsent(whisky.text, mutableListOf(whisky.getAttribute("href")))
                 ?.add(whisky.getAttribute("href"))
 
         try {
-            browser.findElementByXPath("//button[@name='allow']")?.click()
+            browser.findElement(By.xpath("//button[@name='allow']"))?.click()
         } catch (e: Exception) {
             // Clicking allow button was not required.
         }
@@ -59,8 +60,8 @@ class WhiskyCrawler {
     private fun readWishCollectionPair(url: String): Pair<Int, Int> {
         browser[url]
         try {
-            val wish = browser.findElementByXPath("(//div[@id='whisky-community']//button)[1]")
-            val coll = browser.findElementByXPath("(//div[@id='whisky-community']//button)[3]")
+            val wish = browser.findElement(By.xpath("(//div[@id='whisky-community']//button)[1]"))
+            val coll = browser.findElement(By.xpath("(//div[@id='whisky-community']//button)[3]"))
             val wishCount = wish?.text?.replace(Regex("\\D"), "")?.toInt()
             val collCount = coll?.text?.replace(Regex("\\D"), "")?.toInt()
             return Pair(wishCount ?: 0, collCount ?: 0)

eda94bf08c77d9921da6ed321a97994b21d2ba3a
Elmar Brauch <elmar.brauch@telekom.de>
2022-10-08 00:03:18 +0200

    Optimized for demo

--- src/main/kotlin/de/bsi/whisky/WhiskyApplication.kt
+++ src/main/kotlin/de/bsi/whisky/WhiskyApplication.kt
@@ -9,6 +9,6 @@ class WhiskyApplication
 fun main(args: Array<String>) {
 	val context = runApplication<WhiskyApplication>(*args)
 	val crawler = context.getBean(WhiskyCrawler::class.java)
-	crawler.collectDataFor("Johnnie Walker Gold Label")
-	crawler.collectDataFor("Alrik")
+	crawler.collectDataFor("Johnnie Walker Gold Label Reserve Master")
+	crawler.collectDataFor("Alrik 1912")
 }
--- src/main/kotlin/de/bsi/whisky/WhiskyCrawler.kt
+++ src/main/kotlin/de/bsi/whisky/WhiskyCrawler.kt
@@ -2,6 +2,7 @@ package de.bsi.whisky
 
 import org.apache.logging.log4j.LogManager
 import org.openqa.selenium.By
+import org.openqa.selenium.Keys
 import org.openqa.selenium.chrome.ChromeDriver
 import org.springframework.stereotype.Service
 import javax.annotation.PreDestroy
@@ -35,14 +36,18 @@ class WhiskyCrawler {
     }
 
     private fun searchWhiskies(whiskyName: String): Map<String, List<String>> {
-        browser.get("https://whiskybase.com/search?q=%s".format(whiskyName))
+        browser.get("https://whiskybase.com")
+        val searchField = browser.findElement(By.id("search-input"))
+        searchField.sendKeys(whiskyName)
+        searchField.sendKeys(Keys.ENTER)
+
         val map = mutableMapOf<String, MutableList<String>>()
         for(whisky in browser.findElements(By.xpath("//div[@class='row']//a[@class='clickable']")))
             map.putIfAbsent(whisky.text, mutableListOf(whisky.getAttribute("href")))
                 ?.add(whisky.getAttribute("href"))
 
         try {
-            browser.findElement(By.xpath("//button[@name='allow']"))?.click()
+            browser.findElement(By.xpath("//button[@name='allow']")).click()
         } catch (e: Exception) {
             // Clicking allow button was not required.
         }
